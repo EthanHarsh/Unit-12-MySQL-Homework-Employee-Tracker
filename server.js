0f23527d7322442656e2b7f3400f3ef93bcc6a7a
Ethan Harsh <ethan@ethanharsh.com>
2021-07-26 17:44:42 +0000

    core loops

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv').config()
 const mysql = require('mysql2');
 const commands = require('./lib/sqlCommands');
+const inq = require('inquirer');
+const questions = require('./lib/questions');
 
 
 var connection = mysql.createConnection({
@@ -16,20 +18,69 @@ connection.connect(function(err) {
       return;
     }
    
-    console.log('connected as id ' + connection.threadId); 
+    console.log('DB connected as id ' + connection.threadId);
+    console.log(`Let's get started...`);
+    init(); 
 });
 
-try {
-  connection.query(
-    commands.create,
-      (err, results) => {
-        if(err) {
-          console.error(err);
-          return
-        }
-        console.log('Schema created successfully');
+console.log('Hello! 👋 😄');
+console.log('Welcome to the employee management systeminator 5000');
+
+function init() {
+  inq.prompt(questions.init)
+  .then((answers) => {
+    //console.log(answers.start);
+    answers = answers.start;
+    if(answers === 'Continue with data') {
+      //console.log('continue')
+      mainLoop();
+    } else {
+      //console.log('else')
+      clearTable();
+      mainLoop();
+    }
+  })
+}
+
+function mainLoop() {
+  inq.prompt(questions.main)
+    .then((answer) => {
+      if(answer.choice === 'View info') {
+        viewLoop();
+      } else {
+        editLoop();
       }
-    )
-} catch(err) {
-  console.error(err);
+    })
+}
+
+function viewLoop() {
+  inq.prompt(questions.view)
+    .then((answer) => {
+      console.log(answer);
+    })
+}
+
+function editLoop() {
+  inq.prompt(questions.edit)
+    .then((answer) => {
+      console.log(answer);
+    })
+}
+
+function clearTable() {
+  //console.log('clearTable')
+  try {
+    connection.query(
+      commands.create,
+        (err, results) => {
+          if(err) {
+            console.error(err);
+            return
+          }
+          console.log('Schema created successfully');
+        }
+      )
+  } catch(err) {
+    console.error(err);
+  }
 }
